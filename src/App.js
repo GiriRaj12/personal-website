6454b378ece5e7bf230c904266118383804d383a
Giri Raj <giri.raj@presidio.com>
2020-09-04 14:34:36 +0530

    Personal website started building

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './constants';
+import { content } from './constants';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="images">
+        </div>
+        <div className="dev-details">
+          <div className="header-content">
+              <p className="welcome-content">Hi there, Welcome ! </p>
+          </div>
+          <div className="body-content">
+                <p>{content}</p>
+          </div>
+          <div className="line-break">
+          </div>
+          <div className="body-icons">
+                <img alt="timer" src={require("./assets/angular.svg")}></img>
+                <img alt="timer" src={require("./assets/java (2).svg")}></img>
+                <img alt="timer" src={require("./assets/js.svg")}></img>
+                <img alt="timer" src={require("./assets/docker.svg")}></img>
+                <img alt="timer" src={require("./assets/python.svg")}></img>
+                <img alt="timer" src={require("./assets/sass.svg")}></img>
+                <img alt="timer" src={require("./assets/typescript.svg")}></img>
+                <img alt="timer" src={require("./assets/react.svg")}></img>
+            </div>
+          <div className="footer-content-links">
+          <div className="line-break">
+          </div>
+          <div>
+            <a className ="social-links" href="www.github.com/giriraj12" target="_blank"><img alt="timer" src={require("./assets/github.svg")}></img></a>
+            <a className ="social-links" href="www.linkedin.com/in/giriraj-12" target="_blank"><img alt="timer" src={require("./assets/linkedin.svg")}></img></a>
+          </div>
+          </div>
+        </div>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const content = "I am Giri , I am a full stack developer from India. I love building cross platform, high efficient, low latency and highly scalable software applications. Do you think I am your type ?  Lets connect over languages !";
